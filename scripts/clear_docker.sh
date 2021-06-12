35c188a5d547eb1334f054fc5e2d82c4df245cad
HippopoStar <leocabanes@wanadoo.fr>
2021-06-12 14:52:33 +0100

    MaJ "clear_docker.sh"

--- scripts/clear_docker.sh
+++ scripts/clear_docker.sh
@@ -1,36 +1,54 @@
 #!/bin/bash
 
+declare COLOR_BLACK='\033[30m'
+declare COLOR_RED='\033[31m'
+declare COLOR_GREEN='\033[32m'
+declare COLOR_YELLOW='\033[33m'
+declare COLOR_BLUE='\033[34m'
+declare COLOR_MAGENTA='\033[35m'
+declare COLOR_CYAN='\033[36m'
+declare COLOR_WHITE='\033[37m'
+
+declare COLOR_BRIGHT='\033[01m'
+declare EOC='\033[00m'
+
+declare COMMAND_COLOR="`echo "${COLOR_YELLOW}"`"
+
 aux_stop_running_containers () {
 	local READ_ANSWER=''
+	local TARGET_ID=''
 
 	if [ ${#} -eq 1 ] ; then
-		read -n 2 -p "Would you like to stop container [${1}]? [y/n] " READ_ANSWER
+		echo -e "Would you like to ${COLOR_BRIGHT}stop${EOC} the following ${COLOR_BRIGHT}container${EOC}:"
+		read -n 2 -p "[${1}]? [y/n] " READ_ANSWER
 		if [ "x${READ_ANSWER}" = 'xy' ] ; then
-			docker stop "${1}"
+			TARGET_ID="`echo "${1}" | awk '{ print $1 }'`"
+			docker container stop "${TARGET_ID}"
 		fi
 	fi
 }
 
 stop_running_containers () {
-	local RUNNING_CONTAINERS="`docker ps --filter "status=running" | awk 'NR > 1 { print $NF }'`"
+	local DOCKER_COMMAND='docker container ls --filter status=running'
+	local DOCKER_COMMAND_RETURN_VALUE="`${DOCKER_COMMAND}`"
+	local DOCKER_COMMAND_RETURN_VALUE_HEADLESS="`echo "${DOCKER_COMMAND_RETURN_VALUE}" | tail --lines=+2`"
 	local LINE=''
-	local -a ARRAY=()
+	local -a LINES_ARRAY=()
 	local ARRAY_LENGTH=0
 	local ITERATOR=0
 
-	if [ "x${RUNNING_CONTAINERS}" = 'x' ] ; then
-		echo 'No currently running container(s)'
-	else
-		echo 'Currently running container(s):'
-		echo "${RUNNING_CONTAINERS}"
+	echo -e "${COMMAND_COLOR}${DOCKER_COMMAND}${EOC}"
+	echo "${DOCKER_COMMAND_RETURN_VALUE}"
+	echo ''
+	if [ ! \( "x${DOCKER_COMMAND_RETURN_VALUE_HEADLESS}" = 'x' \) ] ; then
 		while IFS= read -r LINE
 		do
-			ARRAY[${ARRAY_LENGTH}]="${LINE}"
+			LINES_ARRAY[${ARRAY_LENGTH}]="${LINE}"
 			ARRAY_LENGTH=$((ARRAY_LENGTH + 1))
-		done <<< "${RUNNING_CONTAINERS}"
-		#echo "RUNNING_CONTAINER(S)_ARRAY: ${ARRAY[@]}"
+		done <<< "${DOCKER_COMMAND_RETURN_VALUE_HEADLESS}"
+		#echo "RUNNING_CONTAINER(S)_ARRAY: ${LINES_ARRAY[@]}"
 		while [ ${ITERATOR} -lt ${ARRAY_LENGTH} ] ; do
-			aux_stop_running_containers "${ARRAY[${ITERATOR}]}"
+			aux_stop_running_containers "${LINES_ARRAY[${ITERATOR}]}"
 			ITERATOR=$((ITERATOR + 1))
 		done
 	fi
@@ -38,36 +56,40 @@ stop_running_containers () {
 
 aux_remove_exited_containers () {
 	local READ_ANSWER=''
+	local TARGET_ID=''
 
 	if [ ${#} -eq 1 ] ; then
-		read -n 2 -p "Would you like to remove container [${1}]? [y/n] " READ_ANSWER
+		echo -e "Would you like to ${COLOR_BRIGHT}remove${EOC} the following ${COLOR_BRIGHT}container${EOC}:"
+		read -n 2 -p "[${1}]? [y/n] " READ_ANSWER
 		if [ "x${READ_ANSWER}" = 'xy' ] ; then
-			docker rm "${1}"
+			TARGET_ID="`echo "${1}" | awk '{ print $1 }'`"
+			docker container rm "${TARGET_ID}"
 		fi
 	fi
 }
 
 remove_exited_containers () {
 	# https://docs.docker.com/engine/reference/commandline/ps/#filtering
-	local EXITED_CONTAINERS="`docker ps --filter "status=exited" | awk 'NR > 1 { print $NF }'`"
+	local DOCKER_COMMAND='docker container ls --filter status=exited'
+	local DOCKER_COMMAND_RETURN_VALUE="`${DOCKER_COMMAND}`"
+	local DOCKER_COMMAND_RETURN_VALUE_HEADLESS="`echo "${DOCKER_COMMAND_RETURN_VALUE}" | tail --lines=+2`"
 	local LINE=''
-	local -a ARRAY=()
+	local -a LINES_ARRAY=()
 	local ARRAY_LENGTH=0
 	local ITERATOR=0
 
-	if [ "x${EXITED_CONTAINERS}" = 'x' ] ; then
-		echo 'No exited container(s)'
-	else
-		echo 'Exited container(s):'
-		echo "${EXITED_CONTAINERS}"
+	echo -e "${COMMAND_COLOR}${DOCKER_COMMAND}${EOC}"
+	echo "${DOCKER_COMMAND_RETURN_VALUE}"
+	echo ''
+	if [ ! \( "x${DOCKER_COMMAND_RETURN_VALUE_HEADLESS}" = 'x' \) ] ; then
 		while IFS= read -r LINE
 		do
-			ARRAY[${ARRAY_LENGTH}]="${LINE}"
+			LINES_ARRAY[${ARRAY_LENGTH}]="${LINE}"
 			ARRAY_LENGTH=$((ARRAY_LENGTH + 1))
-		done <<< "${EXITED_CONTAINERS}"
-		#echo "EXITED_CONTAINER(S)_ARRAY: ${ARRAY[@]}"
+		done <<< "${DOCKER_COMMAND_RETURN_VALUE_HEADLESS}"
+		#echo "EXITED_CONTAINER(S)_ARRAY: ${LINES_ARRAY[@]}"
 		while [ ${ITERATOR} -lt ${ARRAY_LENGTH} ] ; do
-			aux_remove_exited_containers "${ARRAY[${ITERATOR}]}"
+			aux_remove_exited_containers "${LINES_ARRAY[${ITERATOR}]}"
 			ITERATOR=$((ITERATOR + 1))
 		done
 	fi
@@ -75,27 +97,31 @@ remove_exited_containers () {
 
 aux_remove_docker_images () {
 	local READ_ANSWER=''
+	local TARGET_ID=''
 
 	if [ ${#} -eq 1 ] ; then
-		read -n 2 -p "Would you like to remove docker image [${1}]? [y/n] " READ_ANSWER
+		echo -e "Would you like to ${COLOR_BRIGHT}remove${EOC} the following ${COLOR_BRIGHT}image${EOC}:"
+		read -n 2 -p "[${1}]? [y/n] " READ_ANSWER
 		if [ "x${READ_ANSWER}" = 'xy' ] ; then
-			docker image rm "${1}"
+			TARGET_ID="`echo "${1}" | awk '{ print $3 }'`"
+			docker image rm "${TARGET_ID}"
 		fi
 	fi
 }
 
 remove_docker_images () {
-	local DOCKER_IMAGES="`docker image ls | awk 'NR > 1 { print $1":"$2 }'`"
+	local DOCKER_COMMAND='docker image ls'
+	local DOCKER_COMMAND_RETURN_VALUE="`${DOCKER_COMMAND}`"
+	local DOCKER_COMMAND_RETURN_VALUE_HEADLESS="`echo "${DOCKER_COMMAND_RETURN_VALUE}" | tail --lines=+2`"
 	local LINE=''
-	local -a ARRAY=()
+	local -a LINES_ARRAY=()
 	local ARRAY_LENGTH=0
 	local ITERATOR=0
 
-	if [ "x${DOCKER_IMAGES}" = 'x' ] ; then
-		echo 'No docker image(s)'
-	else
-		echo 'Docker image(s):'
-		echo "${DOCKER_IMAGES}"
+	echo -e "${COMMAND_COLOR}${DOCKER_COMMAND}${EOC}"
+	echo "${DOCKER_COMMAND_RETURN_VALUE}"
+	echo ''
+	if [ ! \( "x${DOCKER_COMMAND_RETURN_VALUE_HEADLESS}" = 'x' \) ] ; then
 		# Following commented block of code is from a first draft,
 		# and was not kept due to security issues it can engender
 		#for ELEMENT in ${DOCKER_IMAGES} ; do
@@ -103,12 +129,12 @@ remove_docker_images () {
 		#done
 		while IFS= read -r LINE
 		do
-			ARRAY[${ARRAY_LENGTH}]="${LINE}"
+			LINES_ARRAY[${ARRAY_LENGTH}]="${LINE}"
 			ARRAY_LENGTH=$((ARRAY_LENGTH + 1))
-		done <<< "${DOCKER_IMAGES}"
-		#echo "DOCKER_IMAGE(S)_ARRAY: ${ARRAY[@]}"
+		done <<< "${DOCKER_COMMAND_RETURN_VALUE_HEADLESS}"
+		#echo "DOCKER_IMAGE(S)_ARRAY: ${LINES_ARRAY[@]}"
 		while [ ${ITERATOR} -lt ${ARRAY_LENGTH} ] ; do
-			aux_remove_docker_images "${ARRAY[${ITERATOR}]}"
+			aux_remove_docker_images "${LINES_ARRAY[${ITERATOR}]}"
 			ITERATOR=$((ITERATOR + 1))
 		done
 	fi
@@ -116,48 +142,74 @@ remove_docker_images () {
 
 aux_remove_docker_volumes () {
 	local READ_ANSWER=''
+	local TARGET_ID=''
 
 	if [ ${#} -eq 1 ] ; then
-		read -n 2 -p "Would you like to remove docker volume [${1}]? [y/n] " READ_ANSWER
+		echo -e "Would you like to ${COLOR_BRIGHT}remove${EOC} the following ${COLOR_BRIGHT}volume${EOC}:"
+		read -n 2 -p "[${1}]? [y/n] " READ_ANSWER
 		if [ "x${READ_ANSWER}" = 'xy' ] ; then
-			docker volume rm "${1}"
+			TARGET_ID="`echo "${1}" | awk '{ print $2 }'`"
+			docker volume rm "${TARGET_ID}"
 		fi
 	fi
 }
 
 remove_docker_volumes () {
-	local DOCKER_VOLUMES="`docker volume ls | awk 'NR > 1 { print $2 }'`"
+	local DOCKER_COMMAND='docker volume ls'
+	local DOCKER_COMMAND_RETURN_VALUE="`${DOCKER_COMMAND}`"
+	local DOCKER_COMMAND_RETURN_VALUE_HEADLESS="`echo "${DOCKER_COMMAND_RETURN_VALUE}" | tail --lines=+2`"
 	local LINE=''
-	local -a ARRAY=()
+	local -a LINES_ARRAY=()
 	local ARRAY_LENGTH=0
 	local ITERATOR=0
 
-	if [ "x${DOCKER_VOLUMES}" = 'x' ] ; then
-		echo 'No docker volume(s)'
-	else
-		echo 'Docker volume(s):'
-		echo "${DOCKER_VOLUMES}"
-		echo 'Too see docker s volumes along with their respective disk usage, open a new terminal and run `sudo ls --size --human-readable --format=single-column /var/lib/docker/volumes/`'
+	echo -e "${COMMAND_COLOR}${DOCKER_COMMAND}${EOC}"
+	echo "${DOCKER_COMMAND_RETURN_VALUE}"
+	echo ''
+	echo '(Too see docker s volumes along with their respective disk usage, open a new terminal and run:'
+	echo -e "\`${COLOR_BRIGHT}sudo ls --size --human-readable --format=single-column /var/lib/docker/volumes/${EOC}\`)"
+	echo ''
+	if [ ! \( "x${DOCKER_COMMAND_RETURN_VALUE_HEADLESS}" = 'x' \) ] ; then
 		while IFS= read -r LINE
 		do
-			ARRAY[${ARRAY_LENGTH}]="${LINE}"
+			LINES_ARRAY[${ARRAY_LENGTH}]="${LINE}"
 			ARRAY_LENGTH=$((ARRAY_LENGTH + 1))
-		done <<< "${DOCKER_VOLUMES}"
-		#echo "DOCKER_VOLUME(S)_ARRAY: ${ARRAY[@]}"
+		done <<< "${DOCKER_COMMAND_RETURN_VALUE_HEADLESS}"
+		#echo "DOCKER_VOLUME(S)_ARRAY: ${LINES_ARRAY[@]}"
 		while [ ${ITERATOR} -lt ${ARRAY_LENGTH} ] ; do
-			aux_remove_docker_volumes "${ARRAY[${ITERATOR}]}"
+			aux_remove_docker_volumes "${LINES_ARRAY[${ITERATOR}]}"
 			ITERATOR=$((ITERATOR + 1))
 		done
 	fi
 }
 
 main () {
-	echo "To fast clear your docker environnement from its containers, images and volumes, run this script the following way: \`yes | ${0}\`"
-	echo 'If you deployed a docker constellation through docker-compose, make sure you executed `docker-compose down` before running this script'
+	echo -e "To fast clear your docker environnement from its containers, images and volumes, run this script the following way: \`${COLOR_BRIGHT}yes | ${0}${EOC}\`"
+	echo -e "If you deployed a docker constellation through docker-compose, consider executing \`${COLOR_BRIGHT}docker-compose down${EOC}\` before running this script"
 	stop_running_containers
 	remove_exited_containers
 	remove_docker_images
 	remove_docker_volumes
+
+	echo ''
+	local DOCKER_COMMAND=''
+	DOCKER_COMMAND='docker container ls --all'
+	echo -e "${COMMAND_COLOR}${DOCKER_COMMAND}${EOC}"
+	echo "`${DOCKER_COMMAND}`"
+	echo ''
+	DOCKER_COMMAND='docker network ls'
+	echo -e "${COMMAND_COLOR}${DOCKER_COMMAND}${EOC}"
+	echo "`${DOCKER_COMMAND}`"
+	echo ''
+	#DOCKER_COMMAND='docker node ls'
+	#echo -e "${COMMAND_COLOR}${DOCKER_COMMAND}${EOC}"
+	#echo "`${DOCKER_COMMAND}`"
+	#echo ''
+	#DOCKER_COMMAND='docker service ls'
+	#echo -e "${COMMAND_COLOR}${DOCKER_COMMAND}${EOC}"
+	#echo "`${DOCKER_COMMAND}`"
+	#echo ''
+	DOCKER_COMMAND=''
 }
 
 main
