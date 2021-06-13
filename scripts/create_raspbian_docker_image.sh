fea9c7bf589696532faa7e88c9473892b4eb2f0a
HippopoStar <leocabanes@wanadoo.fr>
2021-06-13 14:38:53 +0100

    Slight improvements on "create_raspbian_docker_image.sh"

--- scripts/create_raspbian_docker_image.sh
+++ scripts/create_raspbian_docker_image.sh
@@ -7,8 +7,8 @@
 # https://hub.docker.com/layers/mongo/library/mongo/bionic/images/sha256-c740f5fce802628d8837ad78d443dda66eecc3db7e8143943c5494a86fe57d7c?context=explore
 # https://github.com/docker-library/mongo/blob/master/4.4/Dockerfile
 
-COLOR_BRIGHT="$(echo -e "\033[01m")"
-EOC="$(echo -e "\033[00m")"
+COLOR_BRIGHT="$(echo "\033[01m")"
+EOC="$(echo "\033[00m")"
 
 RASPBIAN_IMAGE_ARCHIVE_NAME='2021-05-07-raspios-buster-armhf-lite'
 RASPBIAN_IMAGE_ARCHIVE_DOWNLOAD_ADDRESS="https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-05-28/${RASPBIAN_IMAGE_ARCHIVE_NAME}.zip"
@@ -16,9 +16,15 @@ TEMPORARY_DIRECTORY='/tmp/raspbian_docker_tmp'
 HOME_DOWNLOADS_DIRECTORY_NAME='Downloads'
 RASPBIAN_IMAGE_MOUNT_POINT='/mnt/img'
 
+TIMEZONE="$(cat /etc/timezone)"
+#TIMEZONE='Europe/Paris'
+DOCKER_IMPORT_COMMIT_MSG="Image generated using \"$(echo "${0}" | xargs basename)\" on $(TZ="${TIMEZONE}" date +'%F at %T GMT%:z')"
+#echo "${DOCKER_IMPORT_COMMIT_MSG}"
+
 main () {
 	local RASPBIAN_DISK_IMAGE_PARTITION_TABLE=''
-	local RASPBIAN_DISK_IMAGE_LINUX_OFFSET=''
+	local RASPBIAN_DISK_IMAGE_LINUX_PARTITION_SECTORS_OFFSET=''
+	local RASPBIAN_DISK_IMAGE_LINUX_PARTITION_BYTES_OFFSET=''
 
 	# Voir https://www.raspberrypi.org/software/operating-systems/#raspberry-pi-os-32-bit
 	# Section 'Raspberry Pi OS Lite'
@@ -42,11 +48,14 @@ main () {
 	RASPBIAN_DISK_IMAGE_PARTITION_TABLE="$(fdisk -l "${TEMPORARY_DIRECTORY}/${RASPBIAN_IMAGE_ARCHIVE_NAME}.img")"
 	echo "${COLOR_BRIGHT}RASPBIAN_DISK_IMAGE_PARTITION_TABLE${EOC}:"
 	echo "${RASPBIAN_DISK_IMAGE_PARTITION_TABLE}"
-	RASPBIAN_DISK_IMAGE_LINUX_OFFSET="$(echo "${RASPBIAN_DISK_IMAGE_PARTITION_TABLE}" | grep -e 'Linux$' | awk '{ print $2 }')"
-	echo "${COLOR_BRIGHT}RASPBIAN_DISK_IMAGE_LINUX_OFFSET${EOC}:"
-	echo "${RASPBIAN_DISK_IMAGE_LINUX_OFFSET}"
+	RASPBIAN_DISK_IMAGE_LINUX_PARTITION_SECTORS_OFFSET="$(echo "${RASPBIAN_DISK_IMAGE_PARTITION_TABLE}" | grep -e 'Linux$' | awk '{ print $2 }')"
+	echo "${COLOR_BRIGHT}RASPBIAN_DISK_IMAGE_LINUX_PARTITION_SECTORS_OFFSET${EOC}:"
+	echo "${RASPBIAN_DISK_IMAGE_LINUX_PARTITION_SECTORS_OFFSET}"
+	RASPBIAN_DISK_IMAGE_LINUX_PARTITION_BYTES_OFFSET="$((RASPBIAN_DISK_IMAGE_LINUX_PARTITION_SECTORS_OFFSET * 512))"
+	echo "${COLOR_BRIGHT}RASPBIAN_DISK_IMAGE_LINUX_PARTITION_BYTES_OFFSET${EOC}:"
+	echo "${RASPBIAN_DISK_IMAGE_LINUX_PARTITION_BYTES_OFFSET}"
 	sudo mkdir --parents --verbose "${RASPBIAN_IMAGE_MOUNT_POINT}"
-	sudo mount -o loop,offset="$((RASPBIAN_DISK_IMAGE_LINUX_OFFSET * 512))" "${TEMPORARY_DIRECTORY}/${RASPBIAN_IMAGE_ARCHIVE_NAME}.img" "${RASPBIAN_IMAGE_MOUNT_POINT}"
+	sudo mount -o loop,offset="${RASPBIAN_DISK_IMAGE_LINUX_PARTITION_BYTES_OFFSET}" "${TEMPORARY_DIRECTORY}/${RASPBIAN_IMAGE_ARCHIVE_NAME}.img" "${RASPBIAN_IMAGE_MOUNT_POINT}"
 	cat <<-EOF | sudo chroot "${RASPBIAN_IMAGE_MOUNT_POINT}"
 		#apt-get update && apt-get upgrade -y
 		#rm /etc/apt/sources.list
@@ -56,11 +65,14 @@ main () {
 	echo "Running ${COLOR_BRIGHT}tar${EOC} command..."
 	sudo tar cf "${TEMPORARY_DIRECTORY}/docker_image_raspbian.tar" -C "${RASPBIAN_IMAGE_MOUNT_POINT}" ./
 	echo "Running ${COLOR_BRIGHT}docker import${EOC} command..."
-	docker import "${TEMPORARY_DIRECTORY}/docker_image_raspbian.tar" raspbian:latest
+	echo "${DOCKER_IMPORT_COMMIT_MSG}"
+	docker import -m "${DOCKER_IMPORT_COMMIT_MSG}" "${TEMPORARY_DIRECTORY}/docker_image_raspbian.tar" raspbian:latest
 	sudo umount -v "${RASPBIAN_IMAGE_MOUNT_POINT}"
 	sudo rm --dir --interactive=once --verbose "${RASPBIAN_IMAGE_MOUNT_POINT}"
 	echo "${COLOR_BRIGHT}docker image ls${EOC}"
 	docker image ls
+	echo "${COLOR_BRIGHT}docker image history raspbian:latest${EOC}"
+	docker image history raspbian:latest
 }
 
 main
